33b394daf3a125b28b7e1b281d6b92f48c2c04d2
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-02 10:03:05 +0700

    15

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.interface';
 
 @Injectable()
 export class UserService {
+ private users: User[] = [];
   test(): string[] {
     return [];
+  }
+  findAll(): User[] { 
+    return this.users; 
   }
 }
