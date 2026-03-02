6b71b61873b40f96d0540b6c634e8e7c0c4f321a
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-02 09:48:37 +0700

    12

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,12 +6,3 @@ export class AppService {
     return 'Welcome to NestJS!';
   }
 }
-//  import { Injectable } from '@nestjs/common';
-
-// // @Injectable()
-// // export class AppService {
-// //   getHello(): string {
-// //     return `'Welcome to NestJS!'`;
-// //   }
-
-// // }
