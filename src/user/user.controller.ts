e630aaf454d2259de4bd9b756384fe8b62cfaf34
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-02 10:25:59 +0700

    19 late

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,7 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('test')
   test(): string[] {
     return this.userService.test();
   }
