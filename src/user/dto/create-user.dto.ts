d5e7c18f0f7e650dd8c49daca334a5f18e79e901
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-24 01:18:12 +0700

    25

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,11 +3,11 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
-  firstname: string;
+  firstName: string;
 
   @IsNotEmpty()
   @IsString()
-  lastname: string;
+  lastName: string;
 
   @IsOptional()
   @IsString()
