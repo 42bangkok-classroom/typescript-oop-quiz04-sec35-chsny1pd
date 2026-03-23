4fad52635ce30b73daf1111581d50b9a454f1b70
PD <168636679+chsny1pd@users.noreply.github.com>
2026-03-24 00:46:49 +0700

    12

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -9,9 +9,7 @@ export class CreateUserDto {
   @IsString()
   lastname: string;
 
-  @IsEmail()
   email: string;
 
-  @IsString()
   username: string;
 }
